58f78b98a5deec34f5571bdcbc75e79ef2f390bb
joelyakubu269 <joelyakubu269@gmail.com>
2026-07-01 16:50:04 +0100

    my first commit

--- github/formatter.go
+++ github/formatter.go
@@ -1,1 +1,30 @@
 package github
+
+import (
+	"fmt"
+)
+
+func Formatter(events []Event) error {
+	if len(events) == 0 {
+		return fmt.Errorf("no activity found")
+	}
+	data := ""
+	for _, event := range events {
+		switch event.Type {
+		case "PushEvent":
+			No_Count := len(event.Payload.Commits)
+			data = fmt.Sprintf("pushed %d commits to %s", No_Count, event.Repo.Name)
+		case "IssuesEvent":
+			data = fmt.Sprintf("%s a new issue in %s", event.Payload.Action, event.Repo.Name)
+		case "WatchEvent":
+			data = fmt.Sprintf("starred %s ", event.Repo.Name)
+		case "ForkEvent":
+			data = fmt.Sprintf("starred %s ", event.Repo.Name)
+		case "CreateEvent":
+			data = fmt.Sprintf("created %s in %s ", event.Payload.RefType, event.Repo.Name)
+
+		default:
+			data = fmt.Sprintf("%s ")
+		}
+	}
+}
