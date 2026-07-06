1cbb94cdf1568dd8cbff39fce5b228808438be78
joelyakubu269 <joelyakubu269@gmail.com>
2026-07-06 15:52:19 +0100

    made some changes

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,17 @@ import (
 )
 
 func main() {
+	flag.Usage = func() {
+		fmt.Println("GitHub User Activity CLI")
+		fmt.Println()
+		fmt.Println("Usage:")
+		fmt.Println("  go run ./cmd -username=<github-username>")
+		fmt.Println()
+		fmt.Println("Example:")
+		fmt.Println("  go run ./cmd -username=joelyakubu269")
+		fmt.Println()
+		flag.PrintDefaults()
+	}
 	var user string
 	flag.StringVar(&user, "username", "kamranahmedse", "username of github user")
 	flag.Parse()
